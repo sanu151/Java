6b43aee093e5293a92faed8d6f03d35efae291bb
Supriyo Das <68671274+sanu151@users.noreply.github.com>
2024-10-10 19:15:23 +0530

    Comments in Java

---
+++ 4_Comments.java
@@ -0,0 +1,14 @@
+public class Comments{
+    public static void main(String[] args){
+        // Single line Comment
+        
+        /* This is
+        Multi line Commnet */
+        
+        /**
+         * This is javadoc commnet
+        */
+        
+        System.out.println("Commnets in Java");
+    }
+}
