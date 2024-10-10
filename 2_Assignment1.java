41cd4f641716a85449d01527baaef4a5e9a72027
Supriyo Das <68671274+sanu151@users.noreply.github.com>
2024-10-10 18:58:46 +0530

    Assignment 1

---
+++ 2_Assignment1.java
@@ -0,0 +1,8 @@
+// Assignment 1
+public class Product{
+    public static void main (String[] args) {
+        System.out.println("id: 101");
+        System.out.println("product: iphone15");
+        System.out.println("color: white");
+    }
+}
