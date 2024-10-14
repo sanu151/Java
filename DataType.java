096f48bc44893a0b1998bfe6a48c45c9279cc502
Supriyo Das <68671274+sanu151@users.noreply.github.com>
2024-10-14 15:43:55 +0530

    Create DataType.java

---
+++ DataType.java
@@ -0,0 +1,17 @@
+public class DataType{
+    public static void main (String[] args) {
+        boolean b = true;
+        char c = 'a';
+        short s = 11;
+        int i = 9830;
+        float f = 98.30f;
+        double d = 1987365.45;
+        
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("s = "+s);
+        System.out.println("i = "+i);
+        System.out.println("f = "+f);
+        System.out.println("d = "+d);
+    }
+}
